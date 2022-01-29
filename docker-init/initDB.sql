4a4228e2be5460e1bd75452e5646cbe5d1c8cbb3
um4ru-ch4n <17515560@mail.ru>
2022-01-29 14:09:58 +0300

    [WIP]: get ChatInfo and polls

--- docker-init/initDB.sql
+++ docker-init/initDB.sql
@@ -1,27 +1,25 @@
 -- CREATE SEQUENCE user_id_seq;
 CREATE TABLE "user" (
-    user_id INTEGER NOT NULL,
+    user_id BIGINT NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
     firstname VARCHAR(255),
     lastname VARCHAR(255),
-    chat_id INTEGER DEFAULT 0,
+    chat_id BIGINT DEFAULT 0,
     PRIMARY KEY (user_id)
 );
--- ALTER SEQUENCE user_id_seq OWNED BY user.user_id;
 
+-- ALTER SEQUENCE user_id_seq OWNED BY user.user_id;
 CREATE TABLE chat (
-    chat_id INTEGER NOT NULL,
-    head_person INTEGER,
-    poll INTEGER,
+    chat_id BIGINT NOT NULL,
+    poll VARCHAR(255),
     PRIMARY KEY (chat_id)
 );
 
 CREATE TABLE user_chat (
-    user_id INTEGER NOT NULL,
-    chat_id INTEGER NOT NULL,
+    user_id BIGINT NOT NULL,
+    chat_id BIGINT NOT NULL,
+    is_head BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (user_id, chat_id),
-    FOREIGN KEY (user_id)
-        REFERENCES "user" (user_id),
-    FOREIGN KEY (chat_id)
-        REFERENCES chat (chat_id)
+    FOREIGN KEY (user_id) REFERENCES "user" (user_id),
+    FOREIGN KEY (chat_id) REFERENCES chat (chat_id)
 );
--- repository/repository.go
+++ repository/repository.go
@@ -1,7 +1,12 @@
 package repository
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/um4aru-ch4n/healthBot/domain"
 )
 
 type Repository struct {
@@ -12,4 +17,23 @@ func NewRepository(psqlPool *pgxpool.Pool) *Repository {
 	return &Repository{
 		psqlPool: psqlPool,
 	}
+}
+
+func (r *Repository) GetChatInfoPoll(ctx context.Context) (chatInfo map[int64]*domain.ChatInfo, pollChat map[string]int64, err error) {
+	tx, err := r.psqlPool.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return nil, nil, fmt.Errorf("BeginTx: %w", err)
+	}
+
+	const query = `
+	`
+
+	rows, err := tx.Query(ctx, query)
+	if err != nil {
+		return nil, nil, fmt.Errorf("tx.Query: %w", err)
+	}
+
+	defer rows.Close()
+
+	return nil, nil, nil
 }
